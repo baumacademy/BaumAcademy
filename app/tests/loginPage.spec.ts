c922946a43c7b1388ab22598d91296352c043547
monica-2024 <monica.baum24@gmail.com>
2024-12-12 21:12:11 -0500

    empty message profile

--- app/tests/loginPage.spec.ts
+++ app/tests/loginPage.spec.ts
@@ -25,6 +25,37 @@ test.describe("login page", () => {
     //     await expect(value).toBeVisible
     // }
   });
+  //   Today's automation test cases 12/12/2024
+
+  // 1 - cannot update user's profile with empty input(all fields), and expect to see the error message
+  // 2 - gender can be "male" "female, "Male", "Female" otherwise expect to see the error message
+  // 3 - go to view courses page, select a course, click enroll course button and expect to update user's
+  // course data
+  // });
+  test("should expect the error message wwhen profile empty input", async ({
+    page,
+  }) => {
+    const editButton = page.getByRole("button", { name: "Edit" });
+    await expect(editButton).toBeVisible();
+    await editButton.click();
+    await page.pause();
+
+    const locationInput = page.getByTestId("profile-city");
+    await expect(locationInput).toBeVisible();
+    await locationInput.fill("");
+    await page.pause();
+
+    const saveButton = page.getByRole("button", { name: "Save" });
+    await expect(saveButton).toBeVisible();
+    await saveButton.click();
+    await page.pause();
+
+    const errorMsg = page.getByText(
+      "fields cannot be empty, please fill out all fields"
+    );
+    await expect(errorMsg).toBeVisible();
+    await page.pause();
+  });
   // test("should login in with valid credentals", async ({ page }) => {
   //   // await page.goto("http://localhost:3000");
   //   // //await login(page);으로도 할수 있다
@@ -64,100 +95,98 @@ test.describe("login page", () => {
   //   await expect(viewAvailButton).toBeVisible();
   // });
 
-  //12/12/2024
-  test("should be able to log out", async ({ page }) => {
-    // await page.goto("http://localhost:3000");
+  // //12/12/2024
+  // test("should be able to log out", async ({ page }) => {
+  //   // await page.goto("http://localhost:3000");
 
-    // await login(page);
+  //   // await login(page);
 
-    const logoutButton = page.getByRole("button", { name: "Log Out" });
-    await expect(logoutButton).toBeVisible();
-    await logoutButton.click();
+  //   const logoutButton = page.getByRole("button", { name: "Log Out" });
+  //   await expect(logoutButton).toBeVisible();
+  //   await logoutButton.click();
 
-    const signupButton = page.getByText("Sign up");
-    await expect(signupButton).toBeVisible();
-  });
+  //   const signupButton = page.getByText("Sign up");
+  //   await expect(signupButton).toBeVisible();
+  // });
 
-  test("should update profile", async ({ page }) => {
-    //login
-    //click edit
-    //expect save btn to be visible
-    //update location to 'New Test City'
-    //press save
-    //expect that 'New Test City' is visible
-    const editButton = page.getByRole("button", { name: "Edit" });
-    await expect(editButton).toBeVisible();
-    await editButton.click();
+  // test("should update profile", async ({ page }) => {
+  //   //login
+  //   //click edit
+  //   //expect save btn to be visible
+  //   //update location to 'New Test City'
+  //   //press save
+  //   //expect that 'New Test City' is visible
+  //   const editButton = page.getByRole("button", { name: "Edit" });
+  //   await expect(editButton).toBeVisible();
+  //   await editButton.click();
 
-    const saveButton = page.getByRole("button", { name: "Save" });
-    await expect(saveButton).toBeVisible();
+  //   const saveButton = page.getByRole("button", { name: "Save" });
+  //   await expect(saveButton).toBeVisible();
 
-    const location = page.getByTestId("profile-city");
-    await expect(location).toBeVisible();
-    await location.fill("New Test City");
+  //   const location = page.getByTestId("profile-city");
+  //   await expect(location).toBeVisible();
+  //   await location.fill("New Test City");
 
-    const gender = page.getByTestId("profile-gender");
-    await expect(gender).toBeVisible();
-    await gender.fill("Female");
+  //   const gender = page.getByTestId("profile-gender");
+  //   await expect(gender).toBeVisible();
+  //   await gender.fill("Female");
 
-    const occupation = page.getByTestId("profile-occupation");
-    await expect(occupation).toBeVisible();
-    await occupation.fill("QA Tester");
+  //   const occupation = page.getByTestId("profile-occupation");
+  //   await expect(occupation).toBeVisible();
+  //   await occupation.fill("QA Tester");
 
-    const batch = page.getByLabel("Batch #:");
-    await expect(batch).toBeVisible();
-    await batch.fill("2025");
+  //   const batch = page.getByLabel("Batch #:");
+  //   await expect(batch).toBeVisible();
+  //   await batch.fill("2025");
 
-    const kidsCoding = page.getByTestId("profile-radio-1");
-    await expect(kidsCoding).toBeVisible();
-    await kidsCoding.check();
+  //   const kidsCoding = page.getByTestId("profile-radio-1");
+  //   await expect(kidsCoding).toBeVisible();
+  //   await kidsCoding.check();
 
-    await saveButton.click();
+  //   await saveButton.click();
 
-    //after click page
+  //   //after click page
 
-    const newLocation = page.getByText("New Test City");
-    await expect(newLocation).toBeVisible();
+  //   const newLocation = page.getByText("New Test City");
+  //   await expect(newLocation).toBeVisible();
 
-    const newGender = page.getByText("Female");
-    await expect(newGender).toBeVisible();
+  //   const newGender = page.getByText("Female");
+  //   await expect(newGender).toBeVisible();
 
-    const newOccupation = page.getByText("QA Tester");
-    await expect(newOccupation).toBeVisible();
+  //   const newOccupation = page.getByText("QA Tester");
+  //   await expect(newOccupation).toBeVisible();
 
-    const newBatch = page.getByText("2025");
-    await expect(newBatch).toBeVisible();
+  //   const newBatch = page.getByText("2025");
+  //   await expect(newBatch).toBeVisible();
 
-    const enrolledCourse = page.getByText("Kids Coding");
-    await expect(enrolledCourse).toBeVisible();
+  //   const enrolledCourse = page.getByText("Kids Coding");
+  //   await expect(enrolledCourse).toBeVisible();
 
-    // const saveButton = getByRole('button', {name: "Save"});
-    // await
-  });
-  test("should render course page", async ({ page }) => {
-    const courses = page.getByRole("button", { name: "View Available Course" });
-    await expect(courses).toBeVisible();
-    await courses.click();
+  //   // const saveButton = getByRole('button', {name: "Save"});
+  //   // await
+  // });
+  // test("should render course page", async ({ page }) => {
+  //   const courses = page.getByRole("button", { name: "View Available Course" });
+  //   await expect(courses).toBeVisible();
+  //   await courses.click();
 
-    const coursePage = page.locator("h1", { hasText: "Courses" });
-    await expect(coursePage).toBeVisible();
-    const QA5 = page.getByText("5months QA bootcamp");
-    await expect(QA5).toBeVisible();
-    const kidsCoding = page.locator("h3", { hasText: "Kids Coding" });
-    await expect(kidsCoding).toBeVisible();
-    const basicComputer = page.getByText(
-      "very basic to advanced computer skills"
-    );
-    await expect(basicComputer).toBeVisible();
-    const koreanClass = page.locator("h3", { hasText: "Korean Class" });
-    await expect(koreanClass).toBeVisible();
+  //   const coursePage = page.locator("h1", { hasText: "Courses" });
+  //   await expect(coursePage).toBeVisible();
+  //   const QA5 = page.getByText("5months QA bootcamp");
+  //   await expect(QA5).toBeVisible();
+  //   const kidsCoding = page.locator("h3", { hasText: "Kids Coding" });
+  //   await expect(kidsCoding).toBeVisible();
+  //   const basicComputer = page.getByText(
+  //     "very basic to advanced computer skills"
+  //   );
+  //   await expect(basicComputer).toBeVisible();
+  //   const koreanClass = page.locator("h3", { hasText: "Korean Class" });
+  //   await expect(koreanClass).toBeVisible();
 
-    const goBackButton = page.getByRole("button", { name: "Go Back" });
-    await expect(goBackButton).toBeVisible();
-    await goBackButton.click();
+  //   const goBackButton = page.getByRole("button", { name: "Go Back" });
+  //   await expect(goBackButton).toBeVisible();
+  //   await goBackButton.click();
 
-    const email = page.getByText("Email");
-    await expect(email).toBeVisible();
-  });
-  // djklfjsdklfjsdklfjsdlk
+  //   const email = page.getByText("Email");
+  //   await expect(email).toBeVisible();
 });
