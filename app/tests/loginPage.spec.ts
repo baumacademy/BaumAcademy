2f62b3f6405c77f801344bfb34408ec1133533d8
Esther Chung <ehchung0215@gmail.com>
2024-12-17 18:45:41 -0500

    add tests

--- app/tests/loginPage.spec.ts
+++ app/tests/loginPage.spec.ts
@@ -23,32 +23,64 @@ test.describe("login page", () => {
   // });
 
   test("should login in with valid credentials", async ({ page }) => {
-    // const loginOutBtn = page.getByRole("button", { name: "Log out" });
-    // await expect(loginOutBtn).toBeVisible();
-    const ediBtn = page.getByRole('button', {name: "edit"})
-    await expect(ediBtn).toBeVisible()
-    await ediBtn.click()
+    await login(page);
 
-    const locationInput = page.getByLabel('location')
-    await locationInput.fill("")
-
-    const saveBtn = page.getByRole('button', {name: "save"})
-    await expect(saveBtn).toBeVisible()
-    await saveBtn.click()
-
-    await expect(page.getByText("fields cannot be empty, please fill out all fields")).toBeVisible()
+    const loginOutBtn = page.getByRole("button", { name: "Log out" });
+    await expect(loginOutBtn).toBeVisible();
   });
 
-//   test.only("URL test, make sure the url is only going towards the user profile", async({page}) => {
-//     // await page.goto("http://localhost:3000/landing/3")
-//     // await page.waitForTimeout(1000)
-//     // await page.waitForTimeout(2000);
-//     // await page.waitForURL('**/landing');
-//     // await expect(page).toHaveURL("http://localhost:3000/landing/13")
-//     // const testUserName = page.locator("h2", {hasText: "Test User"})
-//     // await expect(testUserName).toBeVisible()
-//     // const currentURL = page.url()
-//     // console.log(currentURL)
-//     // await expect(currentURL).to("http://localhost:3000/landing/9")
-// })
+  test("should see error message", async ({ page }) => {
+    const userPage = page.getByRole("button", { name: "Edit" });
+    await expect(userPage).toBeVisible();
+    await userPage.click();
+    const locationInput = page.getByTestId("profile-city");
+    await locationInput.fill("");
+    await expect(locationInput).toBeVisible();
+    const genderInput = page.getByTestId("profile-gender");
+    await genderInput.fill("");
+    await expect(genderInput).toBeVisible();
+    const occupationInput = page.getByTestId("profile-occupation");
+    await occupationInput.fill("");
+    await expect(occupationInput).toBeVisible();
+    const batchInput = page.getByTestId("profile-batch");
+    await batchInput.fill("");
+    await expect(batchInput).toBeVisible();
+    const radio_1 = page.getByTestId("profile-radio-0");
+    await expect(radio_1).not.toBeChecked();
+    const saveButton = page.getByRole("button", { name: "Save" });
+    await expect(saveButton).toBeVisible();
+    await saveButton.click();
+    const errorMsg = page.getByText("Gender can be either Male or Female");
+    await expect(errorMsg).toBeVisible();
+  });
+
+  test("gender should be either male or female", async ({ page }) => {
+    const editButton = page.getByRole("button", { name: "Edit" });
+    await expect(editButton).toBeVisible();
+    await editButton.click();
+    const profile = page.getByTestId("profile-gender");
+    await expect(profile).toBeVisible();
+    await profile.fill("him");
+    const saveButton = page.getByRole("button", { name: "Save" });
+    await expect(saveButton).toBeVisible();
+    await saveButton.click();
+    const errorMsg = page.getByText("Gender can be either Male or Female");
+    await expect(errorMsg).toBeVisible();
+  });
+
+  test("should update users course data", async ({ page }) => {
+    const coursesButton = page.getByRole("button", {
+      name: "View Available Courses",
+    });
+    await expect(coursesButton).toBeVisible();
+    await coursesButton.click();
+    const qa = page.locator("h3", { hasText: "QA" });
+    await expect(qa).toBeVisible();
+    await qa.click();
+    const enrollButton = page.getByRole("button", { name: "Enroll Course" });
+    await expect(enrollButton).toBeVisible();
+    await enrollButton.click();
+    const courseDesc = page.getByText("5months QA bootcamp");
+    await expect(courseDesc).toBeVisible();
+  });
 });
