2922e2488bdce0c080f91db3b64c0296c40b09f0
monica-2024 <monica.baum24@gmail.com>
2024-12-21 12:01:01 -0500

    tests

--- app/tests/loginPage.spec.ts
+++ app/tests/loginPage.spec.ts
@@ -25,37 +25,62 @@ test.describe("login page", () => {
     //     await expect(value).toBeVisible
     // }
   });
+  //12/21/2024
+  //Classmate page Feature pages
+
+  //-search by classmate name
+  //-should only see classmates that are in the user's enrolled class
+  //-click on view profile should how user profile
+  //-go back button works
 
   //12/17/2024
   //create a test case for courses serach engine bar
-
-  test("Search bar gives the correct list of classes", async ({ page }) => {
-    const profile = page.getByRole("button", {
-      name: "View Available Courses",
-    });
-    //Ensure the 'view available courses' button is visible and click it
+  test("Make chages to classmate page feature", async ({ page }) => {
+    //Search enrolled class:QA
+    const enrolledCourse = page.getByText("QA");
+    await expect(enrolledCourse).toBeVisible();
+    //Ensure vie classmates are there and click
+    const profile = page.getByRole("button", { name: "View Classmates" });
     await expect(profile).toBeVisible();
     await profile.click();
-    //Get the search input field and ensure it is visible
-    const searchInput = page.getByPlaceholder(
-      "Search by name or description..."
-    );
+    //Ensure Class QA is visible
+    const qaClass = page.locator("h1", { hasText: "QA" });
+    await expect(qaClass).toBeVisible();
+    //Ensure search bar is visible
+    const searchInput = page.getByPlaceholder("Search Classmates");
     await expect(searchInput).toBeVisible();
-    //Get all the list items before filling in the search input
-    const listsBeforeFill = await page.locator("li").all();
-    console.log("listsBeforeFill", listsBeforeFill.length); //Fix the console log here
-    await expect(listsBeforeFill.length).toBe(4); //Expect 4 items initially
-    //Fill the search input with 'qa'
-    await searchInput.fill("qa");
-    //Get the QA element that should appear after the search
-    const qa = page.locator("h3", { hasText: "QA" });
-    //Get all the list items after filling in the search input
-    const lists = await page.locator("li").all();
-    console.log("lists", lists.length); //Log the new number of ist items
-    //Check if only 1 list item matches the search result
-    await expect(lists.length).toBe(1);
-    await expect(qa).toBeVisible(); //Ensure QA course appears in the list
+    //search by classmate name
+    await searchInput.fill("sophia");
+    await expect(searchInput).toBeVisible();
   });
+  // test("Search bar gives the correct list of classes", async ({ page }) => {
+  //   const profile = page.getByRole("button", {
+  //     name: "View Available Courses",
+  //   });
+  //   //Ensure the 'view available courses' button is visible and click it
+  //   await expect(profile).toBeVisible();
+  //   await profile.click();
+  //   //Get the search input field and ensure it is visible
+  //   const searchInput = page.getByPlaceholder(
+  //     "Search by name or description..."
+  //   );
+  //   await expect(searchInput).toBeVisible();
+  //   //Get all the list items before filling in the search input
+  //   const listsBeforeFill = await page.locator("li").all();
+  //   console.log("listsBeforeFill", listsBeforeFill.length); //Fix the console log here
+  //   await expect(listsBeforeFill.length).toBe(4); //Expect 4 items initially
+  //   //Fill the search input with 'qa'
+  //   await searchInput.fill("qa");
+  //   //Get the QA element that should appear after the search
+  //   const qa = page.locator("h3", { hasText: "QA" });
+  //   //Get all the list items after filling in the search input
+  //   const lists = await page.locator("li").all();
+  //   console.log("lists", lists.length); //Log the new number of ist items
+  //   //Check if only 1 list item matches the search result
+  //   await expect(lists.length).toBe(1);
+  //   await expect(qa).toBeVisible(); //Ensure QA course appears in the list
+  // });
+
   //   Today's automation test cases 12/12/2024
 
   // 1 - cannot update user's profile with empty input(all fields), and expect to see the error message
@@ -63,30 +88,30 @@ test.describe("login page", () => {
   // 3 - go to view courses page, select a course, click enroll course button and expect to update user's
   // course data
   // });
-  test("should expect the error message wwhen profile empty input", async ({
-    page,
-  }) => {
-    const editButton = page.getByRole("button", { name: "Edit" });
-    await expect(editButton).toBeVisible();
-    await editButton.click();
-    await page.pause();
+  // test("should expect the error message wwhen profile empty input", async ({
+  //   page,
+  // }) => {
+  //   const editButton = page.getByRole("button", { name: "Edit" });
+  //   await expect(editButton).toBeVisible();
+  //   await editButton.click();
+  //   await page.pause();
 
-    const locationInput = page.getByTestId("profile-city");
-    await expect(locationInput).toBeVisible();
-    await locationInput.fill("");
-    await page.pause();
+  //   const locationInput = page.getByTestId("profile-city");
+  //   await expect(locationInput).toBeVisible();
+  //   await locationInput.fill("");
+  //   await page.pause();
 
-    const saveButton = page.getByRole("button", { name: "Save" });
-    await expect(saveButton).toBeVisible();
-    await saveButton.click();
-    await page.pause();
+  //   const saveButton = page.getByRole("button", { name: "Save" });
+  //   await expect(saveButton).toBeVisible();
+  //   await saveButton.click();
+  //   await page.pause();
 
-    const errorMsg = page.getByText(
-      "fields cannot be empty, please fill out all fields"
-    );
-    await expect(errorMsg).toBeVisible();
-    await page.pause();
-  });
+  //   const errorMsg = page.getByText(
+  //     "fields cannot be empty, please fill out all fields"
+  //   );
+  //   await expect(errorMsg).toBeVisible();
+  //   await page.pause();
+  // });
   // test("should login in with valid credentals", async ({ page }) => {
   //   // await page.goto("http://localhost:3000");
   //   // //await login(page);으로도 할수 있다
