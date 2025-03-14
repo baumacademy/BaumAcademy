4b8c2fe280a95af3d6740e40f55d13e4cc762ede
seeck3 <marco.dseo@gmail.com>
2025-03-13 20:49:50 -0400

    swagger doc

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
-import { Sequelize, Model, DataTypes } from "sequelize";
+import { Sequelize, Model, DataTypes, Op } from "sequelize";
 import bodyParser from "body-parser";
 import { UserModel } from "./user";
 import cors from "cors";
@@ -9,6 +9,8 @@ import { StudentModel } from "./student";
 import { StudentType, StudentUpdateType } from "./student.model";
 import { ClassModel } from "./class";
 import { hasMissingValues } from "./utils/hasMissingValues";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 dotenv.config();
 
@@ -46,10 +48,26 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.get("/api", async (req: Request, res: Response) => {
-//   const users = await User.findAll();
-//   res.send(users);
-// });
+// Swagger configuration
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Baum Academy API",
+      version: "1.0.0",
+      description: "API documentation for Baum Academy",
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+      },
+    ],
+  },
+  apis: ["./src/index.ts"], // Path to the API docs
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get("/api", async (req: Request, res: Response) => {
   const students = await Student.findAll({
@@ -66,10 +84,69 @@ app.get("/api", async (req: Request, res: Response) => {
   res.send(students);
 });
 
+/**
+ * @swagger
+ * /api:
+ *   get:
+ *     summary: Retrieve a list of students
+ *     responses:
+ *       200:
+ *         description: A list of students
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   firstName:
+ *                     type: string
+ *                   lastName:
+ *                     type: string
+ *                   occupation:
+ *                     type: string
+ *                   city:
+ *                     type: string
+ *                   class:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                       subject:
+ *                         type: string
+ *                       content:
+ *                         type: string
+ */
+
 app.get("/api/classes", async (req: Request, res: Response) => {
   const classes = await Class.findAll();
   res.send({ classes });
 });
+
+/**
+ * @swagger
+ * /api/classes:
+ *   get:
+ *     summary: Retrieve a list of classes
+ *     responses:
+ *       200:
+ *         description: A list of classes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   subject:
+ *                     type: string
+ *                   content:
+ *                     type: string
+ */
 
 app.get("/api/classes/:classId", async (req: Request, res: Response) => {
   const { classId } = req.params;
@@ -85,6 +162,46 @@ app.get("/api/classes/:classId", async (req: Request, res: Response) => {
   });
   res.send({classMates: classMates?.students, subject: classMates?.subject})
 });
+
+/**
+ * @swagger
+ * /api/classes/{classId}:
+ *   get:
+ *     summary: Retrieve a class by ID
+ *     parameters:
+ *       - in: path
+ *         name: classId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A class object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 classMates:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                       firstName:
+ *                         type: string
+ *                       lastName:
+ *                         type: string
+ *                       occupation:
+ *                         type: string
+ *                       city:
+ *                         type: string
+ *                       email:
+ *                         type: string
+ *                 subject:
+ *                   type: string
+ */
 
 app.post("/api/student/create", async (req: Request, res: Response) => {
   const salt = bcrypt.genSaltSync(SALT_ROUNDS);
@@ -114,6 +231,40 @@ app.post("/api/student/create", async (req: Request, res: Response) => {
   });
 });
 
+/**
+ * @swagger
+ * /api/student/create:
+ *   post:
+ *     summary: Create a new student
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Student created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ */
+
 app.post("/api/student/login", async (req: Request, res: Response) => {
   const getUser = await Student.findOne({ where: { email: req.body.email } });
   const user = await getUser?.toJSON();
@@ -140,6 +291,41 @@ app.post("/api/student/login", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/student/login:
+ *   post:
+ *     summary: Login a student
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     userId:
+ *                       type: integer
+ *                 success:
+ *                   type: boolean
+ */
+
 app.get("/api/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   const user = await Student.findOne({
@@ -165,8 +351,54 @@ app.get("/api/:id", async (req: Request, res: Response) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/{id}:
+ *   get:
+ *     summary: Retrieve a student by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A student object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                     firstName:
+ *                       type: string
+ *                     lastName:
+ *                       type: string
+ *                     occupation:
+ *                       type: string
+ *                     city:
+ *                       type: string
+ *                     class:
+ *                       type: object
+ *                       properties:
+ *                         id:
+ *                           type: integer
+ *                         subject:
+ *                           type: string
+ *                         content:
+ *                           type: string
+ *                 success:
+ *                   type: boolean
+ */
+
 app.patch("/api/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
   const student: StudentUpdateType = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -189,7 +421,61 @@ app.patch("/api/:id", async (req: Request, res: Response) => {
     message: "Student updated successfully",
     success: true,
   });
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+/**
+ * @swagger
+ * /api/{id}:
+ *   patch:
+ *     summary: Update a student by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               occupation:
+ *                 type: string
+ *               city:
+ *                 type: string
+ *               gender:
+ *                 type: string
+ *               batch:
+ *                 type: string
+ *               class:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *     responses:
+ *       200:
+ *         description: Student updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ */
 
 app.patch("/api/:id/course", async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -202,6 +488,251 @@ app.patch("/api/:id/course", async (req: Request, res: Response) => {
     success: true,
   });
 });
+
+/**
+ * @swagger
+ * /api/{id}/course:
+ *   patch:
+ *     summary: Update a student's course by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               classId:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Student updated course successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ */
+
+// Search students by firstName, lastName, or email
+app.get("/api/search/students", async (req: Request, res: Response) => {
+  const { query } = req.query;
+  const students = await Student.findAll({
+    where: {
+      [Op.or]: [
+        { firstName: { [Op.like]: `%${query}%` } },
+        { lastName: { [Op.like]: `%${query}%` } },
+        { email: { [Op.like]: `%${query}%` } },
+      ],
+    },
+    include: [
+      {
+        model: Class,
+        as: "class",
+        attributes: ["id", "subject", "content"],
+      },
+    ],
+  });
+  res.send(students);
+});
+
+/**
+ * @swagger
+ * /api/search/students:
+ *   get:
+ *     summary: Search students by firstName, lastName, or email
+ *     parameters:
+ *       - in: query
+ *         name: query
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: A list of students
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   firstName:
+ *                     type: string
+ *                   lastName:
+ *                     type: string
+ *                   email:
+ *                     type: string
+ *                   occupation:
+ *                     type: string
+ *                   city:
+ *                     type: string
+ *                   class:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                       subject:
+ *                         type: string
+ *                       content:
+ *                         type: string
+ */
+
+app.get("/api/search/classes", async (req: Request, res: Response) => {
+  const { query } = req.query;
+  const classes = await Class.findAll({
+    where: {
+      [Op.or]: [
+        { subject: { [Op.like]: `%${query}%` } },
+        { content: { [Op.like]: `%${query}%` } },
+      ],
+    }
+  });
+  res.send(classes);
+});
+
+/**
+ * @swagger
+ * /api/search/classes:
+ *   get:
+ *     summary: Search classes by subject or content
+ *     parameters:
+ *       - in: query
+ *         name: query
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: A list of classes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   subject:
+ *                     type: string
+ *                   content:
+ *                     type: string
+ */
+
+app.post("/api/classes/create", async (req: Request, res: Response) => {
+  const newClass = {
+    subject: req.body.subject,
+    content: req.body.content,
+  };
+
+  const createdClass = await Class.create(newClass);
+  return res.send({
+    message: "Class created successfully",
+    class: createdClass,
+    success: true,
+  });
+});
+
+/**
+ * @swagger
+ * /api/classes/create:
+ *   post:
+ *     summary: Create a new class
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               subject:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Class created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 class:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                     subject:
+ *                       type: string
+ *                     content:
+ *                       type: string
+ *                 success:
+ *                   type: boolean
+ */
+
+app.patch("/api/classes/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const updatedClass = {
+    subject: req.body.subject,
+    content: req.body.content,
+  };
+
+  await Class.update(updatedClass, { where: { id } });
+  return res.send({
+    message: "Class updated successfully",
+    success: true,
+  });
+});
+
+/**
+ * @swagger
+ * /api/classes/{id}:
+ *   patch:
+ *     summary: Update a class by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               subject:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Class updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ */
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
